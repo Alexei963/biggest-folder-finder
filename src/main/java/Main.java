52756a2be5ed232453738bc7f739b016b7445887
Aleksey Pomendyukov <alexei963@yandex.ru>
2023-01-11 17:11:04 +0400

    Adding a class FolderSizeCalculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,18 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
 
   public static void main(String[] args) {
-    String folderPath = "D:/Programs/Java";
+    String folderPath = "D:/Learning";
     File file = new File(folderPath);
-    System.out.printf("Размер папки %s = %s", folderPath, getFolderSize(file));
+    long start = System.currentTimeMillis();
+    FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+    ForkJoinPool pool = new ForkJoinPool();
+    long folderSize = pool.invoke(calculator);
+    System.out.printf("Размер папки %s = %s\n", folderPath, folderSize);
+    long duration = System.currentTimeMillis() - start;
+    System.out.println("Время выполнения: " + duration);
 
   }
   public static long getFolderSize(File folder) {
@@ -14,6 +21,7 @@ public class Main {
     }
     long sum = 0;
     File[] files = folder.listFiles();
+    assert files != null;
     for (File file : files) {
       sum += getFolderSize(file);
     }
